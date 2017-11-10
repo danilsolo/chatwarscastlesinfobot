b9f20766ba37dcf480ba38cbd16c00be97450fef
danilsolo <danilsolo123@gmail.com>
2017-11-10 20:13:50 +0300

    db

--- main.py
+++ main.py
@@ -2,10 +2,26 @@
 import telebot
 import logging
 import datetime
+import sqlite3
 
 logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)-3s]# %(levelname)-5s [%(asctime)s] %(message)s'
                     , level = logging.INFO)
 
+# class a:
+#     pass
+#
+# info = a()
+#
+# bots = dict()
+# bots['infobotred']
+# bots['infobotmint']
+# bots['infobotblack']
+# bots['infobotwhite']
+# bots['infobotblue']
+# bots['infobottwilight']
+# bots['infobotyellow']
+#
+# bots = (infobotred, infobotmint, infobotblack, infobotwhite, infobotblue, infobottwilight, infobotyellow)
 
 def niceprint(string):
     tabindex = 0
@@ -25,6 +41,7 @@ def niceprint(string):
     return out
 
 
+
 bot = telebot.TeleBot('454423390:AAEU3fgrrqTYPquwzilge9AfP6Dd47X-5rQ')
 
 castles = []
@@ -36,13 +53,15 @@ def getinfo(message):
     global castles
     global out
     logging.debug(niceprint(str(message)))
-    logging.info(str(message.from_user.username) + ': ' + message.text)
+    logging.debug(str(message.from_user.username) + ': ' + message.text)
 
-    if datetime.datetime.fromtimestamp(message.date) < datetime.datetime.now()-datetime.timedelta(minutes=1):
+    if datetime.datetime.fromtimestamp(message.date) < datetime.datetime.now()-datetime.timedelta(seconds=20):
         logging.info('Cтарое сообщение')
         return
 
     if '💬Чат замка: ссылка' in message.text and message.forward_from.id == 265204902:
+        logging.info(str(message.from_user.username) + ' прислал сообщение о замке' )
+
 
         # bot.send_message('@chatwarscastleinfo', message.text[0:130])
         logging.debug(message.text[:2])
@@ -51,17 +70,17 @@ def getinfo(message):
 
 
         if castle in castles:
-            bot.send_message(message.chat.id, 'Давай еще замков остались:' + str(set(allcastles)-set(castles)))
+            bot.send_message(message.chat.id, 'Твой уже есть давай еще замков остались:' + str(set(allcastles)-set(castles)))
         else:
             castles.append(castle)
 
             castleinfo = message.text.splitlines()
-            castlename = castleinfo[0]
-            castletime = castleinfo[1]
-            castleweather = castleinfo[2]
+            castlename = castleinfo[0][:2]
+            castletime = castleinfo[1][:-2]
+            castleweather = castleinfo[2][7:]
             castleres = castleinfo[castleinfo.index('Казна замка:') + 1]
 
-            out += castlename + '\n' + castletime + '\n' + castleweather + '\n ' + castleres + '\n\n'
+            out += castlename + castletime + castleweather + castleres + '\n'
 
             logging.debug(out)
 
@@ -73,7 +92,6 @@ def getinfo(message):
             castles = []
 
 
-print(datetime.datetime.minute)
 
 
 if __name__ == '__main__':
